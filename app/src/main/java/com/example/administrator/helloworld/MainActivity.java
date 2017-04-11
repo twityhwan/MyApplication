a97b9f1885ee95052f666d1f2edae741d7f12cf3
twityhwan <muri1004@naver.com>
2017-04-11 21:34:35 +0900

    20170411 Menu example

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView txtView = (TextView)findViewById(R.id.tv1);
+        registerForContextMenu(txtView);
 
         Button btn = (Button)findViewById(R.id.btn1);
 
@@ -23,5 +27,68 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 1, Menu.NONE, "First");
+        menu.add(Menu.NONE, 2, Menu.NONE, "Second");
+        menu.add(Menu.NONE, 3, Menu.NONE, "Third");
+        return true;
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item1 = menu.findItem(1);
+
+        if (item1.isEnabled() == true)
+            item1.setEnabled(false);
+        else
+            item1.setEnabled(true);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case 1:
+                // FIrst (항목 ID 1) 이벤트
+                Toast.makeText(this, "First Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case 2:
+                 // Second (항목 ID 2) 이벤트
+                Toast.makeText(this, "Second Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case 3:
+                // Third (항목 ID 3) 이벤트
+                Toast.makeText(this, "Third Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        switch(v.getId()) {
+            case R.id.tv1:
+                menu.clear();
+                menu.add(Menu.NONE, 1, Menu.NONE, "First");
+                menu.add(Menu.NONE, 2, Menu.NONE, "Second");
+                menu.add(Menu.NONE, 3, Menu.NONE, "Third");
+                break;
+        }
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case 1:
+                Toast.makeText(this, "First Context Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case 2:
+                Toast.makeText(this, "Second Context Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case 3:
+                Toast.makeText(this, "Third Context Menu is selected", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
     }
 }
